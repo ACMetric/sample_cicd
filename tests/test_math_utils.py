dc665166f1fb52d8cf1a5bb0faf07fb8e42e3adb
nils <nils.j@adc-consulting.com>
2025-07-23 13:46:38 +0200

    fix: oops, forgot to install pre-commit. now it should work!

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,12 +1,8 @@
-from app.math_utils import add
-from app.math_utils import subtract
+from app.math_utils import add, subtract
 
 
 def test_add():
     assert add(2, 3) == 5
-
-
-
 
 
 def test_subtract():
